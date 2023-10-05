447e77a2f22e49cc399e919883579511eaf7d861
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-10-05 13:04:40 -0500

    Move duplicate removal algorithm from node auditor with tests.

--- src/util/index.ts
+++ src/util/index.ts
@@ -1,5 +1,6 @@
 import { getConfiguration, getConfigurationFromUrl } from "./configuration.js";
 import { convertMavenDependencyToLicense } from "./converters.js";
+import { removeDuplicates } from "./duplicate-remover.js";
 import { LicenseOutputter, MetadataOutputter } from "./outputters.js";
 import { getCurrentVersionString, getRootProjectName } from "./root-project.js";
 
@@ -7,6 +8,7 @@ export type { LicenseOutputter, MetadataOutputter };
 
 export {
   convertMavenDependencyToLicense,
+  removeDuplicates,
   getConfiguration,
   getConfigurationFromUrl,
   getCurrentVersionString,
