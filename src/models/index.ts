ff0534c215affdc9a9ba5e7a426e5435afba1d27
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-10-19 15:46:29 -0500

    Remove unused configuration object. Replaced by common lib.

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,9 +1,7 @@
-import { Configuration } from "./configuration.js";
 import { GradleDependency, GradleDependencyReport } from "./gradle.js";
 import { MavenDependency, MavenLicense } from "./maven.js";
 
 export type {
-  Configuration,
   GradleDependency,
   GradleDependencyReport,
   MavenDependency,
